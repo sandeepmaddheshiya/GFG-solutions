ae5aa8fd8d968c9299f9dcf4a0d287c0a6bed804
Sandeep Maddheshiya <113048079+Itz-Sandy@users.noreply.github.com>
2024-01-04 23:04:32 +0530

    first commit

---
+++ Day 4/Print_Linked_List_elements.java
@@ -0,0 +1,25 @@
+
+/* Node is defined as
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Print_Linked_List_elements
+{
+    // Print elements of a linked list on console
+    // head pointer input could be NULL as well
+    // for empty list
+    void display(Node head)
+    {
+        //add code here.
+        while(head!=null){
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
+    }
+}
